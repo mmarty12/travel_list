73c3d8eee2fadc514034c7483b31d105fe88638b
Masha Martyniuk <mmartyniuk004@gmail.com>
2023-11-20 15:53:59 +0200

    implemented statistics display

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ export default function App() {
       <Logo />
       <Form onAddItems={handleAddItems} />
       <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -93,10 +93,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className='stats'>
+        <em>Start adding some items to your packing list!</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className='stats'>
-      <em>You have X items on your list, and you've already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? 'You got everything ready to go! ✈'
+          : ` You have ${numItems} items on your list, and you've already packed ${numPacked} (${percentage}
+        %)`}
+      </em>
     </footer>
   );
 }
