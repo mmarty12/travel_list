3f1654e9f90d07b3c3e3a89d402a3f9a28d86df7
Masha Martyniuk <mmartyniuk004@gmail.com>
2023-11-20 16:16:37 +0200

    implementing 3 types of list sorting
    - sorting by the input order
    - sorting by the description
    - sorting by the packed status

--- src/App.js
+++ src/App.js
@@ -70,13 +70,29 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState('input');
+
+  let sortedItems;
+  if (sortBy === 'input') sortedItems = items;
+  if (sortBy === 'description')
+    sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === 'packed')
+    sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className='list'>
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />
         ))}
       </ul>
+      <div className='actions'>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value='input'>Sort by the input order</option>
+          <option value='description'>Sort by the description</option>
+          <option value='packed'>Sort by the packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
